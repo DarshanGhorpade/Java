c70c264b160c7134cc285585f893456772c4ecad
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 22:53:15 +0530

    Update question3.java

--- 8. Access Modifiers/question3.java
+++ 8. Access Modifiers/question3.java
@@ -5,3 +5,42 @@
     package
     Access the PROTECTED fields and methods from any class in different package
 */
+
+package p1;
+
+class A
+{
+  protected int count;
+    
+  protected void display(){
+		System.out.println("Count = "+ this.count);
+	}
+
+  public static void main(String[]args){
+    A obj = new A();
+    obj.count = 0;
+
+    obj.display();
+  }// End main
+}//End class A
+
+package p1;
+public class question3 extends A{
+    public static void main(String[] args){
+        question3 obj = new question3();
+	      obj.count = 0;
+        obj.display();
+    }//End main
+}// End question3 class
+
+
+// C.java is in different package
+package p2;
+import p1.*;
+public class C extends A{
+    public static void main(String[] args){
+        C obj = new C();
+	    obj.count = 0;
+        obj.display();
+    }//End main
+}//End class C
