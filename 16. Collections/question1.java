631fd96272a0ac6841f228cbeb4f42f94109a09b
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 20:18:05 +0530

    Create question1.java

---
+++ 16. Collections/question1.java
@@ -0,0 +1,14 @@
+/*
+    1. Create an ArrayList of type String with 10 string elements. Add 10 string elements to
+    ArrayList and perform the below operations
+    Add an element to the ArrayList
+    Iterate through the ArrayList by using Iterator object
+    Add an element at a specific index
+    Remove an element from the ArrayList, Remove at an index
+    Update the element at a specific index
+    Check the element is present at a particular index
+    Get an element at a particular index
+    Find out the size of the ArrayList
+    Check the given element is present in the ArrayList
+    Remove all elements of the ArrayList
+*/
