0afeebf37e5e3206977e9df94d20de688b3df539
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 10:39:44 +0530

    Create question1.java

---
+++ 6. Strings/question1.java
@@ -0,0 +1,15 @@
+/* 1. Different ways of creating string */
+public class question1
+{
+	public static void main(String[] args) {
+	    
+	    // 1. creating String using String literal
+	    String s1 = "Hello";
+	    
+	    // 2. creating String using new keyword
+	    String s2 = new String("World");
+	    
+		System.out.println(s1);
+		System.out.println(s2);
+	}// End main
+}// End question1 
