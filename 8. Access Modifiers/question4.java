b6c13b930fa251e6bd0cc716c3d45153dea98c85
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 22:55:41 +0530

    Update question4.java

--- 8. Access Modifiers/question4.java
+++ 8. Access Modifiers/question4.java
@@ -3,3 +3,33 @@
       Access the public methods and fields from any class in the same package or different
       package.
 */
+
+package p1;
+
+public class A
+{
+  public int count;
+    
+  public void display(){
+		System.out.println("Count = "+ this.count);
+	}
+
+  public static void main(String[]args){
+    A obj = new A();
+    obj.count = 0;
+
+    obj.display();
+  }//End main
+}//End class A
+
+
+//question4.java
+
+package p1;
+public class question4 extends A{
+    public static void main(String[] args){
+        question4 obj = new question4();
+	    obj.count = 0;
+        obj.display();
+    }//End main
+}//End question4 class
