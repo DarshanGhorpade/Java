7d5c1d9d092e0e795119468c8e5b161e7859defe
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 22:12:00 +0530

    Update question1.java

--- 8. Access Modifiers/question1.java
+++ 8. Access Modifiers/question1.java
@@ -1,2 +1,28 @@
 // 1. Create a class with PRIVATE fields, private method and a main method. Print the fields in main method. Call the private method in main method 
 //    Create a sub class and try to access the private fields and methods from sub class.
+
+// A.java
+
+package p;
+class A
+{
+    int count;
+    
+    void display(){
+		System.out.println("Hello World");
+	}
+	
+}
+
+// question1.java
+
+package p1;
+public class question1 {
+    public static void main(String args[]) {
+        A obj = new A();
+        
+        obj.count = 0;
+        
+        obj.display();
+    }// End main
+}//End question1 class
