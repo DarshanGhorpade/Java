e7d5fdbfd44c79cad7f83ee2a9a5237ff46cb2ca
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:56:17 +0530

    Update question5.java

--- 10. Interfaces/question5.java
+++ 10. Interfaces/question5.java
@@ -1,1 +1,21 @@
 // 5. Create two interfaces with the same method (same signature) in both the interfaces. Implement these two interfaces in one class. Call the method.
+
+interface A{
+  void f();
+}
+
+interface B{
+  void f();
+}
+
+class question5 implements A, B{   
+  @Override
+  public void f() {
+    System.out.println("Override interface method");
+  }
+
+  public static void main(String... args){   
+    question5 obj = new question5();
+    obj.f();
+  }//End main
+}//End question5 class
