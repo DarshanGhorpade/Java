5f2e3641fd41c6f698edb4e0c43d48a4563d6ba2
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 10:52:52 +0530

    Create question4.java

---
+++ 6. Strings/question4.java
@@ -0,0 +1,10 @@
+// 4. Extract a string using substring 
+
+public class question4
+{
+	public static void main(String[] args) {
+        String s = "Java Programming";
+        
+		System.out.println(s.substring(0,4));
+	}// End main
+}// End question4 class
