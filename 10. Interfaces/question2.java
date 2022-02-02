8eacc4e4c6377417a911280d0205e731e9f3368a
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:38:30 +0530

    Update question2.java

--- 10. Interfaces/question2.java
+++ 10. Interfaces/question2.java
@@ -1,1 +1,25 @@
 // 2. Create an interface with two methods, but implement only one in a class. Call the method implemented.
+
+// Interface
+interface Animal {
+  public void animalSound(); // interface method (does not have a body)
+  public void sleep(); // interface method (does not have a body)
+}
+
+// Pig "implements" the Animal interface
+class Pig implements Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class question2 {
+  public static void main(String[] args) {
+    Pig myPig = new Pig();  // Create a Pig object
+    myPig.animalSound();
+  }//End main
+}//End question2 class
+
+// will not compile
+// Error will occur: error: Pig is not abstract and does not override abstract method sleep() in Animal
