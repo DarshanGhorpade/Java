654e5eaf3e7758f5d875449a5f6305e9fc10d9af
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:15:36 +0530

    Update question4.java

--- 9. Abstract Class/question4.java
+++ 9. Abstract Class/question4.java
@@ -1,1 +1,26 @@
 // 4. Create an instance for the child class in child class and call non-abstract methods
+
+abstract class Shape {
+
+    //abstract method
+    abstract void display();
+    //non-abstract method    
+    public void a(){
+        System.out.println("Calling non-abstract method by child class object");
+    }
+
+}
+
+public class question4 extends Shape {
+
+    @Override
+    public void display() { System.out.println("Child"); }
+
+    public static void main(String[] args)
+    {
+        question4 obj = new question4();
+        obj.display();
+        obj.a();
+    }//End main
+
+}//End question4 class
