7c246bbfcadeba28973c306aaa2d328d0b797e99
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:44:15 +0530

    Update question3.java

--- 10. Interfaces/question3.java
+++ 10. Interfaces/question3.java
@@ -1,1 +1,21 @@
 // 3. Use Interface instances to call the implemented method in the implemented class
+
+interface A{
+	void fun();
+}
+
+class question3 implements A{
+	public void fun(){
+		System.out.println("fun()");
+	}
+  
+  public static void main(String[] args){
+		A b = new A();
+		b.fun();
+	}//End main
+}//End question3 class
+
+// Error will occur
+// error: A is abstract; cannot be instantiated
+//		A b = new A();
+//		      ^
