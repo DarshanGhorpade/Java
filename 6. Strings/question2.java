b824033d3b61da12f42ca94728111020d3a9a9bc
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 10:45:04 +0530

    Create question2.java

---
+++ 6. Strings/question2.java
@@ -0,0 +1,8 @@
+public class question2
+{
+	public static void main(String[] args) {
+        String s1 = "Hello", s2 = "World";
+        String s3 = s1 + s2;
+		    System.out.println(s3);
+	}// End main
+}// End question2 class
