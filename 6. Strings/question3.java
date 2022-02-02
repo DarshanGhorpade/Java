3c9c5e22121d00879be3136ebe90976e9643b11a
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 10:48:39 +0530

    Create question3.java

---
+++ 6. Strings/question3.java
@@ -0,0 +1,12 @@
+// 3. Find length of String
+
+public class question3
+{
+	public static void main(String[] args) {
+        String s = "Hello";
+        // Finding length of string
+        int len = s.length();
+        // Display length of string
+		System.out.println("Length os string s : " + len);
+	}// End main
+}// End question3 class
