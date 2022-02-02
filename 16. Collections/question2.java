c7ad85e18502cb169001c133c4df52058c87415c
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 20:18:42 +0530

    Create question2.java

---
+++ 16. Collections/question2.java
@@ -0,0 +1,14 @@
+ /*
+    2. Create a HashMap with at least 10 key value pairs of the Student ID and Name
+    Insert a Key value mapping into the map
+    Fetch the value of a Key
+    Create a clone/copy of HashMap
+    Check if the given Key is in the Map
+    Check if the value is in the Map
+    Check if the map is empty
+    Print the size of the Map to the console
+    Print all the Keys of the map to the console
+    Print all the Keys of the map to the console
+    Remove a specific Key-value pair
+    Copy all the elements of the Map to another Map
+*/
