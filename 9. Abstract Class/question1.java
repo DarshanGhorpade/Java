12acc6c4b3764d13cb88f8791210841379c0e3a1
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:04:58 +0530

    Update question1.java

--- 9. Abstract Class/question1.java
+++ 9. Abstract Class/question1.java
@@ -1,1 +1,21 @@
 // 1. Create an abstract class with abstract and non-abstract methods.
+
+//Abstract class with abstract and non-abstract methods.
+abstract class Bike{  
+  abstract void run();  
+  void Break(){
+        System.out.println("Break applied safely");
+  }
+}// End Bike  
+
+class question1 extends Bike{  
+    void run(){
+        System.out.println("running safely");
+    }  
+
+    public static void main(String args[]){  
+        Bike obj = new question1();  
+        obj.run();  
+        obj.Break();
+    }// End main
+}// End question1  
