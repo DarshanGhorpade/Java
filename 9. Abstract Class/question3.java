881932f1902acc2054eba703a0cdef7a27eeb784
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:13:12 +0530

    Update question3.java

--- 9. Abstract Class/question3.java
+++ 9. Abstract Class/question3.java
@@ -1,1 +1,21 @@
 // 3. Create an instance for the child class in child class and call abstract methods
+
+abstract class Shape {
+
+    abstract void display();
+
+}
+
+public class question3 extends Shape {
+
+    @Override
+    public void display() { System.out.println("Child"); }
+
+    public static void main(String[] args)
+    {
+        question3 obj = new question3();
+        obj.display();
+
+    }//End main
+
+}//End question3 class
