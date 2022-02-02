ad1368fe140d48526596c968d4cdc428b2da7a18
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:53:19 +0530

    Update question4.java

--- 10. Interfaces/question4.java
+++ 10. Interfaces/question4.java
@@ -1,1 +1,26 @@
 // 4. Create two interfaces with one method each. Implement these two interfaces in one class.
+
+interface A{
+  void f1();
+}
+
+interface B{
+  void f2();
+}
+
+class question4 implements A, B{   
+  @Override
+  public void f1() {  // from interface A
+    System.out.println("Override first interface method");
+  }
+
+  @Override
+  public void f2() {  // from interface B
+    System.out.println("Override second interface method");
+  }
+  public static void main(String... args){   
+    question4 obj = new question4();
+    obj.f1();
+    obj.f2();
+  }//End main
+}//End question4 class
