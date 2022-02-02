859d58fd09bb0be9981487fad984f98f4f1cb111
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:07:12 +0530

    Update question2.java

--- 9. Abstract Class/question2.java
+++ 9. Abstract Class/question2.java
@@ -2,3 +2,24 @@
   2. Create a sub class for an abstract class. Create an object in the child class for the
     abstract class and access the non-abstract methods
 */
+//Abstract class with abstract and non-abstract methods.
+abstract class Bike{  
+  abstract void run();  
+  void Break(){
+        System.out.println("Break applied safely");
+  }
+}// End Bike  
+
+class question2 extends Bike{  
+    void run(){
+        System.out.println("running safely");
+    }  
+
+    public static void main(String args[]){  
+        Bike obj = new question2();
+        //accessing abstract method
+        obj.run();
+        //accessing non-abstract method
+        obj.Break();
+    }// End main
+}// End question2
