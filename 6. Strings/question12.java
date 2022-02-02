01290c0b9c9ea75ccd7d698b03c74092b3ebd6b8
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 19:30:10 +0530

    Create question12.java

---
+++ 6. Strings/question12.java
@@ -0,0 +1,10 @@
+// 12. Converting Numbers to Strings with valueOf()
+
+public class question12
+{
+	public static void main(String[] args) {
+        int value=30;  
+        String s1=String.valueOf(value);  
+        System.out.println(s1+10);//concatenating string with 10 
+	}//End main
+}//End question12 class
