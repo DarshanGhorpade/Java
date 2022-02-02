f32b9c41746dfbd0787d1845df7b2a841c69c0d5
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 11:03:20 +0530

    Create question5.java

---
+++ 6. Strings/question5.java
@@ -0,0 +1,11 @@
+// 5. Searching in String using indexOf() 
+
+public class question5
+{
+	public static void main(String[] args) {
+        String s = "Java Programming";
+        //int index = s.indexOf
+        
+		System.out.println(s.indexOf("va"));
+	}// End main
+}// End question5 class
