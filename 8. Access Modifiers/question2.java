a23cfb0473da2e5b34908ee9d30a2fe530c0d4b1
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 22:12:50 +0530

    Update question2.java

--- 8. Access Modifiers/question2.java
+++ 8. Access Modifiers/question2.java
@@ -1,1 +1,27 @@
 // 2. Create a class with DEFAULT fields and methods. Access these fields and methods from any other class in the same package
+
+// A.java
+
+package p;
+class A
+{
+    int count;
+    
+    void display(){
+		System.out.println("Hello World");
+	}
+	
+}
+
+// question1.java
+
+package p1;
+public class question1 {
+    public static void main(String args[]) {
+        A obj = new A();
+        
+        obj.count = 0;
+        
+        obj.display();
+    }// End main
+}//End question1 class
