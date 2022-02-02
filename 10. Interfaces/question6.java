39631be2ac9db0ea6d4f485f97c008f1111aa626
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 00:00:28 +0530

    Update question6.java

--- 10. Interfaces/question6.java
+++ 10. Interfaces/question6.java
@@ -1,1 +1,16 @@
 // 6. Create an interface with a default method and implement it in a class. Do not provide implementation to the default method and call the method.
+
+
+interface A{
+  default void f(){
+      System.out.println("This is Java");
+  }
+}
+
+class question6 implements A{   
+
+  public static void main(String... args){   
+    question6 obj = new question6();
+    obj.f();
+  }//End main
+}//End question6 class
