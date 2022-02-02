413ee78d59ed62f3598de0e40c3112f26f7edb0f
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 20:06:56 +0530

    Create question8.java

---
+++ 10. Interfaces/question8.java
@@ -0,0 +1,5 @@
+/*
+    8. Create a PUBLIC interface with fields and methods, fields should have values assigned.
+    Implement this interface to some class and print the values of the interface fields and
+    call the interface methods
+*/
