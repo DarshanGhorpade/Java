9a3e0768c63c201d8576d39de13744bb033fd2ce
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 19:12:56 +0530

    Create question9.java

---
+++ 6. Strings/question9.java
@@ -0,0 +1,10 @@
+// 9. Trimming strings with trim()
+
+public class question9
+{
+	public static void main(String[] args) {
+        String s="  hello string   ";  
+        System.out.println(s+"java");//without trim()  
+        System.out.println(s.trim()+"java");//with trim()
+	}// End main
+}// End question9 class
