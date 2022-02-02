4df95730b4651c063fdb7d31371a267d9b749df9
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 19:18:19 +0530

    Create question10.java

---
+++ 6. Strings/question10.java
@@ -0,0 +1,10 @@
+// 10. Replacing characters in strings with replace()
+
+public class question10
+{
+	public static void main(String[] args) {
+        String s="Java is a very good programming language.";  
+        String replaceString=s.replace('a','e');//replaces all occurrences of 'a' to 'e'  
+        System.out.println(replaceString); 
+	}//End main
+}//End question10.java
