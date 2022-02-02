c8b0f3eaab735827ce25b75a2e5f0378cffa67cd
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 20:14:20 +0530

    Create question1.java

---
+++ 12. Constructors/question1.java
@@ -0,0 +1,5 @@
+/*
+    1. Write a class with a default constructor, one argument constructor and two argument
+    constructors. Instantiate the class to call all the constructors of that class from a main
+    class
+*/
