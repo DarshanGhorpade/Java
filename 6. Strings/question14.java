1326dd6b96c614a4dbf6a474572771f9a7c12291
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 19:39:36 +0530

    Create question14.java

---
+++ 6. Strings/question14.java
@@ -0,0 +1,10 @@
+// 14. Converting to uppercase and lowercase
+
+public class question14
+{
+	public static void main(String[] args) {
+        String upStr = "ABCD";
+		    String lowStr = upStr.toLowerCase();	
+		    System.out.println("\nThe Uppercase String  =  " + lowStr);
+	}//End main
+}//End question14 class
