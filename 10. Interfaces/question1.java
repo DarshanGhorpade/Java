99a02fb6ee9b0caf92ac01138ac4a9352df0db7a
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-02 23:22:00 +0530

    Update question1.java

--- 10. Interfaces/question1.java
+++ 10. Interfaces/question1.java
@@ -1,1 +1,22 @@
 // 1. Create an interface with only one method and implement it in a class. Call the method implemented.
+
+// interface
+interface Animal {
+  public void animalSound(); // interface method (does not have a body)
+}
+
+
+// Pig "implements" the Animal interface
+class Pig implements Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class question1 {
+  public static void main(String[] args) {
+    Pig myPig = new Pig();  // Create a Pig object
+    myPig.animalSound();
+  }//End main
+}//End question1 class
