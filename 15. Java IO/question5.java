996d1ee4e181653bc22c7429380d5150646f3533
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:12:10 +0530

    Update question5.java

--- 15. Java IO/question5.java
+++ 15. Java IO/question5.java
@@ -1,1 +1,27 @@
 // 5. Write a program to read text from .txt file using FileReader
+
+import java.io.FileReader;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to read characters from a text file.
+ *
+ */
+ 
+public class question5 {
+ 
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("MyFile.txt");
+            int character;
+ 
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            reader.close();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//End main
+}//End question5 class
