79b6d58a6ffa04db70f66d2919425a9857c882e6
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:23:32 +0530

    Update question5.java

--- 14. Exceptions/question5.java
+++ 14. Exceptions/question5.java
@@ -1,1 +1,14 @@
 // 5. Write a program to throw exception with your own message
+
+class question5 {
+public static void main(String args[]){
+      try {
+          int a = 30, b = 0;
+          int c = a / b; // cannot divide by zero
+          System.out.println("Result = " + c);
+      }
+      catch (Exception e) {
+          System.out.println("ERROR: Divide by zero is illegal!!");
+      }
+	}//End main
+}//End question5 class
