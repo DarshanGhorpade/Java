a90ed6e184764c5db43910e9240d1a56fc5de1df
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:38:09 +0530

    Create question13.java

--- 14. Exceptions/question13.java
+++ 14. Exceptions/question13.java
@@ -1,1 +1,16 @@
 // 13. Write a program to generate NoSuchFieldException
+
+
+import java.io.*;
+import java.util.*;
+
+public class question13{ 
+    public static void main(String[] args){
+        Set exampleleSet = new HashSet();  
+        Hashtable exampleTable = new Hashtable();  
+        exampleleSet.iterator().next();                         //This throws NoSuchElementException  
+        exampleTable.elements().nextElement();      //as there are no elements in our Set  
+                                                                                //and HashTable but we are trying to access the    
+                                                                                          //elements.     
+    }//End main
+}//End question13 class
