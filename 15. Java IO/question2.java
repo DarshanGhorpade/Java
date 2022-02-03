35a2cd1b436acd00a2e221ad68172b6f3b5143cd
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:05:07 +0530

    Update question2.java

--- 15. Java IO/question2.java
+++ 15. Java IO/question2.java
@@ -1,1 +1,31 @@
 // 2. Write a program to write text to .txt file using OutputStream
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to write a text file.
+ *
+ */
+ 
+public class question2 {
+    public static void main(String[] args) {
+        
+        String data = "This is a line of text inside the file.";
+
+        try {
+            FileOutputStream output = new FileOutputStream("MyFile.txt");
+
+            byte[] array = data.getBytes();
+
+            // Writes byte to the file
+            output.write(array);
+
+            output.close();
+        }
+
+        catch(Exception e) {
+            e.getStackTrace();
+        }
+    }//End main
+}//End question2 class
