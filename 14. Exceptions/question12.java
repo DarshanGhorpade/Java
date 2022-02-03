93021b9007d666bf356f0b6d9f47bfe32e12e1fa
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:34:06 +0530

    Update question12.java

--- 14. Exceptions/question12.java
+++ 14. Exceptions/question12.java
@@ -1,1 +1,34 @@
 // 12. Write a program to generate IOException
+
+
+import java.io.*;
+
+public class question12{ 
+    public FileInputStream testMethod(){
+        File file = new File("test.txt");
+        FileInputStream fileInputStream = null;
+        try{
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                if (fileInputStream != null){
+                    fileInputStream.close();
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return fileInputStream;
+    }
+
+    public static void main(String[] args){
+        Myclass obj = new Myclass();
+        obj.testMethod();
+    }//End main
+}//End question12 class
