3393e8b00a78b7d3cb1d27cc809c04414ef0444f
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:18:25 +0530

    Update question10.java

--- 14. Exceptions/question10.java
+++ 14. Exceptions/question10.java
@@ -1,1 +1,16 @@
 // 10. Write a program to generate ClassNotFoundException
+
+//a class called program is defined
+public class question10{ 
+    //main method is called
+    public static void main(String args[]){
+        //class not found exception is defined using try and catch block
+        try{
+            // the forname method in class class looks for the mentioned class
+            Class.forName("Car");
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }   
+    }//End main
+}//End question10 class
