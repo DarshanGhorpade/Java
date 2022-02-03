c88e9c2df4dc56cba5cce3f8b27ef4e26995ca21
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:14:20 +0530

    Update question6.java

--- 15. Java IO/question6.java
+++ 15. Java IO/question6.java
@@ -1,1 +1,26 @@
 // 6. Write a program to write text to .txt file using FileWriter
+
+import java.io.FileWriter;
+
+public class question6 {
+
+  public static void main(String args[]) {
+
+    String data = "This is the data in the output file";
+
+    try {
+      // Creates a FileWriter
+      FileWriter output = new FileWriter("MyFile.txt");
+
+      // Writes the string to the file
+      output.write(data);
+
+      // Closes the writer
+      output.close();
+    }
+
+    catch (Exception e) {
+      e.getStackTrace();
+    }
+  }//End main
+}//End question6 class
