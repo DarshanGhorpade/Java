1dfbdf80a6364241e3ace3998168dc737c61341a
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:58:56 +0530

    Update question17.java

--- 14. Exceptions/question17.java
+++ 14. Exceptions/question17.java
@@ -1,1 +1,16 @@
 // 17. Write a program to generate StringIndexOutOfBoundsException
+
+// Java program to demonstrate NullPointerException
+class question17 {
+public static void main(String args[]){
+        try {
+			String a = "This is like chipping "; // length is 22
+			char c = a.charAt(24); // accessing 25th element
+			System.out.println(c);
+        }
+
+		catch (Exception e) {
+            e.printStackTrace();
+		}
+	}//End main
+}//End question17 class
