50f1d667187f085e4b2d486adf0018a445a1e320
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:30:03 +0530

    Update question4.java

--- 11. this and super/question4.java
+++ 11. this and super/question4.java
@@ -1,1 +1,17 @@
 // 4. Call argument constructor of current class using this()
+
+class A{  
+    A(){  
+        this(5);  
+        System.out.println("hello a");  
+    }  
+    A(int x){  
+        System.out.println(x);  
+    }  
+} 
+
+class question4{  
+    public static void main(String args[]){  
+        A a=new A();  
+    }//End main
+}//End question4 class  
