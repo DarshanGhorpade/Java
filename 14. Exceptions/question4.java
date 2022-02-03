7439ff45ab351986940d6a5dd63654af166bcf77
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:20:43 +0530

    Update question4.java

--- 14. Exceptions/question4.java
+++ 14. Exceptions/question4.java
@@ -1,1 +1,24 @@
 // 4. Write a program with multiple catch blocks
+
+class question4 {
+    
+    public static void main(String args[]){
+        try{    
+            int a[]=new int[5];    
+            a[5]=30/0;    
+        }    
+        catch(ArithmeticException e)  
+          {  
+           System.out.println("Arithmetic Exception occurs");  
+          }    
+        catch(ArrayIndexOutOfBoundsException e)  
+          {  
+           System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+          }    
+        catch(Exception e)  
+          {  
+           System.out.println("Parent Exception occurs");  
+          }             
+        System.out.println("rest of the code");    
+	}//End main
+}//End question4 class
