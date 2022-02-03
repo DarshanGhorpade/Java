ef6d5702440cadb13af52cab6544ee04d58f61a6
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 10:58:08 +0530

    Update question8.java

--- 10. Interfaces/question8.java
+++ 10. Interfaces/question8.java
@@ -3,3 +3,23 @@
     Implement this interface to some class and print the values of the interface fields and
     call the interface methods
 */
+
+//Interface
+public interface Bank{ 
+    int fixDeposit = 100;
+    float rateOfInterest();  
+}
+
+
+//Class implement interface
+class SBI implements Bank{     
+    public float rateOfInterest(){return 9.15f;}  
+}  
+
+class question8{  
+public static void main(String[] args){  
+        Bank b=new SBI();  
+        System.out.println("Fix Deposit: "+b.fixDeposit);  
+        System.out.println("ROI: "+b.rateOfInterest());  
+    }//End main
+}// End question8 class
