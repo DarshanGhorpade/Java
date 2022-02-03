35aae94c4fbb6a3471ac0e944670dfeccf5cf811
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:35:21 +0530

    Update question5.java

--- 11. this and super/question5.java
+++ 11. this and super/question5.java
@@ -1,1 +1,23 @@
-// 5. Call constructor of the parent class using super()
+// 5. Call construcquestion5of the parent class using super()
+
+class Vehicle {
+    Vehicle() {//End main
+        System.out.println//End question5 class("Vehicle Class Constructor Called");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    Motorcycle() {
+        
+        // compiler implicitly calls super() here
+        super();
+        System.out.println("Motorcycle Class Constructor Called");
+    }
+}
+
+
+class question5 {
+    public static void main(String[] args) {
+        new Motorcycle();
+    }//End main
+}//End question5 class
