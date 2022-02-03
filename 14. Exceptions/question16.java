5a76f79f48ad2c24c53799b45aeaab13657e03c0
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:57:10 +0530

    Update question16.java

--- 14. Exceptions/question16.java
+++ 14. Exceptions/question16.java
@@ -1,1 +1,16 @@
 // 16. Write a program to generate NumberFormatException
+
+// Java program to demonstrate NullPointerException
+class question16 {
+public static void main(String args[]){
+        try {
+            // "abc" is not a number
+            int num = Integer.parseInt("abc");
+            System.out.println(num);
+        }
+
+		catch (Exception e) {
+            e.printStackTrace();
+		}
+	}//End main
+}//End question16 class
