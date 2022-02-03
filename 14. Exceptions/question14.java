3ce9c7bd84c998497e721ab1a2bd7f8320f21f23
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:52:34 +0530

    Update question14.java

--- 14. Exceptions/question14.java
+++ 14. Exceptions/question14.java
@@ -1,1 +1,28 @@
 // 14. Write a program to generate NoSuchMethodException
+
+// Java Program to illustrate
+// NoSuchMethodException
+class question14 {
+    public question14{
+		Class i;
+		try {
+			i = Class.forName("java.lang.String");
+			try {
+				Class[] p = new Class[5];
+			}
+			catch (SecurityException e) {
+				e.printStackTrace();
+			}
+			catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			}
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+    public static void main(String[] args){
+		new question14();
+	}//End main
+}//End question14 class
