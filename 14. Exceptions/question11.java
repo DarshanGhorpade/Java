9284f13ca245125832768f13ffa8cb69cf759887
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:22:30 +0530

    Update question11.java

--- 14. Exceptions/question11.java
+++ 14. Exceptions/question11.java
@@ -1,1 +1,19 @@
 // 11. Write a program to generate FileNotFoundException
+
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Myclass{ 
+    //main method is called
+    public static void main(String args[]){
+        try{
+            File file = new File("/home/techvidvan/file.txt");
+            FileReader fileReader = new FileReader(file);
+            System.out.println("Successful");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }//End main
+}//End question11 class
