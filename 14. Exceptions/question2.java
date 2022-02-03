e9a9ffe94dcfc14b6d07349c2ee31ef4c045167e
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:10:11 +0530

    Update question2.java

--- 14. Exceptions/question2.java
+++ 14. Exceptions/question2.java
@@ -1,1 +1,15 @@
 // 2. Handle the Arithmetic exception using try-catch block
+
+// Java program to demonstrate NullPointerException
+class question2 {
+public static void main(String args[]){
+      try {
+          int a = 30, b = 0;
+          int c = a / b; // cannot divide by zero
+          System.out.println("Result = " + c);
+      }
+      catch (Exception e) {
+          e.printStackTrace();
+      }
+	}//End main
+}//End question2 class
