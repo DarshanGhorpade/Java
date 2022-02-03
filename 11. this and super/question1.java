343f06cb15451a01d0cb9b25b7e6465e97d6817a
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:18:09 +0530

    Create question1.java

--- 11. this and super/question1.java
+++ 11. this and super/question1.java
@@ -1,1 +1,24 @@
 // 1. Print the fields/instance members of the current class using this and without using object
+
+class Student{  
+    int rollno;  
+    String name;  
+    float fee;  
+    Student(int rollno,String name,float fee){  
+        this.rollno=rollno;  
+        this.name=name;  
+        this.fee=fee;  
+    }  
+    void display(){
+        System.out.println(this.rollno+" "+this.name+" "+this.fee);
+    }  
+}  
+  
+class question1{  
+    public static void main(String args[]){  
+        Student s1=new Student(111,"ankit",5000f);  
+        Student s2=new Student(112,"sumit",6000f);  
+        s1.display();  
+        s2.display();  
+    }//End main
+}//End question1 class
