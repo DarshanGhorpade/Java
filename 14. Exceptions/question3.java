c35bb1711fdd1b4faec239afe66dc20f5621429e
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:17:06 +0530

    Create question3.java

--- 14. Exceptions/question3.java
+++ 14. Exceptions/question3.java
@@ -1,1 +1,19 @@
 // 3. Write a method which throws exception, Call that method in main class without try block
+
+// Java program to demonstrate NullPointerException
+class question3 {
+    public static int div(int a, int b) throws ArithmeticException{
+        return a/b;
+    }
+    
+    public static void main(String args[]){
+        try {
+            int a = 30, b = 0;
+            int c = div(a,b); // cannot divide by zero
+            System.out.println("Result = " + c);
+        }
+		catch (Exception e) {
+            e.printStackTrace();
+		}
+	}//End main
+}//End question3 class
