a9285d27d7323a66402d11596627431e15511592
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:27:56 +0530

    Update question3.java

--- 11. this and super/question3.java
+++ 11. this and super/question3.java
@@ -1,1 +1,17 @@
 // 3. Call constructor of the current class using this()
+
+class A{  
+    A(){
+        System.out.println("hello a");
+    }  
+    A(int x){  
+        this();  
+        System.out.println(x);  
+    }  
+}  
+
+class question3{  
+    public static void main(String args[]){  
+        A a=new A(10);  
+    }//End main
+}//End question3 class  
