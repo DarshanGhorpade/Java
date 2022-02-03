947fbf174c673136b816876bbdd41860f2508dce
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:48:43 +0530

    Update question1.java

--- 12. Constructors/question1.java
+++ 12. Constructors/question1.java
@@ -3,3 +3,36 @@
     constructors. Instantiate the class to call all the constructors of that class from a main
     class
 */
+
+class A {
+    int a, b;
+    
+    A(){
+        a = 0;
+        b = 0;
+    }
+    A(int x){
+        a = x;
+    }
+    A(int x, int y){
+        a = x;
+        b = y;
+    }
+    
+    void display(){
+        System.out.println("a = "+this.a+" b = "+this.b);
+    }
+}
+
+
+
+public class question1{
+        public static void main(String args[]) {
+            A obj1 = new A();
+            obj1.display();
+            A obj2 = new A(1);
+            obj2.display();
+            A obj3 = new A(2,3);
+            obj3.display();
+        }//End main
+ }//End question1 class
