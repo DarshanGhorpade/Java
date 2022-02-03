559e6340794cf3748a195edf1378d81fee5a3000
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 14:48:53 +0530

    Update question1.java

--- 15. Java IO/question1.java
+++ 15. Java IO/question1.java
@@ -1,1 +1,27 @@
 // 1. Write a program to read text from .txt file using InputStream
+
+import java.io.FileReader;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to read characters from a text file.
+ *
+ */
+ 
+public class question1 {
+ 
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("MyFile.txt");
+            int character;
+ 
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            reader.close();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//End main
+}//End question1 class
