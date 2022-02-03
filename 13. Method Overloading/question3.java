ba3dda76237bc21b07e106e15b2acc7551e98ee4
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 12:53:11 +0530

    Update question3.java

--- 13. Method Overloading/question3.java
+++ 13. Method Overloading/question3.java
@@ -1,1 +1,24 @@
 // 3. Write two methods with the same name and same number of parameters of same type and call from main method
+
+class question3 {
+    
+    void sum (int a, int b){
+        System.out.println(a + " + " + b + " = " +(a+b)) ;
+    }
+
+    void sum (int a, int b){
+        System.out.println(a + " + " + b + " = " +(a+b)) ;
+    }
+  
+    public static void main (String[] args){
+        question3 cal = new question3();
+        cal.sum (8,5);      //compiler error
+    }//End main
+}//End question3 class
+
+
+/*Error:  
+        error: method sum(int,int) is already defined in class Main
+               void sum (int a, int b){
+                    ^
+*/
