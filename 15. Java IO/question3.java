68b23ea85b47859ec373d719712b31d03e8acb8f
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:07:42 +0530

    Update question3.java

--- 15. Java IO/question3.java
+++ 15. Java IO/question3.java
@@ -1,1 +1,32 @@
 // 3. Read text from a .txt file using BufferedInputStream
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+class question3 {
+    public static void main(String[] args) {
+        try {
+
+            // Creates a FileInputStream
+            FileInputStream file = new FileInputStream("MyFile.txt");
+
+            // Creates a BufferedInputStream
+            BufferedInputStream input = new BufferedInputStream(file);
+
+            // Reads first byte from file
+            int i = input .read();
+
+            while (i != -1) {
+                System.out.print((char) i);
+
+                // Reads next byte from the file
+                i = input.read();
+            }
+            input.close();
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }//End main
+}//End question3 class
