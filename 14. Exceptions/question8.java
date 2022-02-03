86c4f27c43351fde001b826a0c29ab01f4cac16e
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:33:47 +0530

    Update question8.java

--- 14. Exceptions/question8.java
+++ 14. Exceptions/question8.java
@@ -1,1 +1,7 @@
 // 8. Write a program to generate Arithmetic Exception
+
+public class question8{
+    public static void main(String[]args){
+        System.out.println("10/0 = "+ 10/0);
+    }//End main
+}//End question8 class
