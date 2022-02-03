4f5b888f5abb038f4db81dd1c3977da118ffab71
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:54:52 +0530

    Update question15.java

--- 14. Exceptions/question15.java
+++ 14. Exceptions/question15.java
@@ -1,1 +1,14 @@
 // 15. Write a program to generate NullPointerException
+
+class question15 {
+public static void main(String args[])
+	{
+		try {
+			String a = null; // null value
+			System.out.println(a.charAt(0));
+		}
+		catch (NullPointerException e) {
+            e.printStackTrace();
+		}
+	}//End main
+}//End question15 class
