83478065437399364516eef6239a46bc976c3259
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:35:41 +0530

    Update question8.java

--- 15. Java IO/question8.java
+++ 15. Java IO/question8.java
@@ -1,1 +1,26 @@
 // 8. Write text to a .txt file using BufferedWriter
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class question8{
+	public static void main(String[] args){
+    String data = "Hello!! Welcome to java.";
+		try {
+            // Creates a FileWriter
+            FileWriter file = new FileWriter("MyFile.txt");
+
+            // Creates a BufferedWriter
+            BufferedWriter output = new BufferedWriter(file);
+
+            // Writes the string to the file
+            output.write(data);
+
+            // Closes the writer
+            output.close();
+        } 
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}//End main
+}//End question8 class
