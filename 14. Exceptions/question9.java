994f95e9e0b5785f162f8a6962b2d76d12c8ecf5
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 20:36:35 +0530

    Update question9.java

--- 14. Exceptions/question9.java
+++ 14. Exceptions/question9.java
@@ -1,1 +1,13 @@
 // 9. Write a program to generate ArrayIndexOutOfBoundException
+
+class question9 {
+    public static void main(String args[]){
+        String[] arr = {"Rohit","Shikar","Virat","Dhoni"};   
+        //Declaring 4 elements in the String array                                       
+        for(int i=0;i<=arr.length;i++) {       
+            //Here, no element is present at the iteration number arr.length, i.e 4  
+            System.out.println(arr[i]);      
+            //So it will throw ArrayIndexOutOfBoundException at iteration 4           
+        }  
+	}//End main
+}//End question9 class
