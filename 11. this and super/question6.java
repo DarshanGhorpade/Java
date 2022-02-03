663a2b31004a799f54b086354629d44e80e109c2
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:39:29 +0530

    Create question6.java

--- 11. this and super/question6.java
+++ 11. this and super/question6.java
@@ -1,1 +1,37 @@
 // 6. Use this() and super() in methods not in constructors
+
+class parent {
+    String str="I am parent";
+    //method of parent Class
+    public void foo() {
+        System.out.println("Hello World " + str);
+    }
+}
+
+class child extends parent {
+    String str="I am child";
+    // different foo implementation in child Class
+    public void foo() {
+        System.out.println("Hello World "+str);
+    }
+
+    // calling the foo method of parent class
+    public void parentClassFoo(){
+        super.foo();
+    }
+
+    // changing the value of str in parent class and calling the foo method of parent class
+    public void fooAgain(){
+        this.foo();
+    }
+}
+
+
+public class question6{
+        public static void main(String args[]) {
+            child obj = new child();
+            obj.foo();
+            obj.parentClassFoo();
+            obj.fooAgain();
+        }//End main
+}//End question6 class
