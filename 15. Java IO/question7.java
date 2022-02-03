501d53c5e6101b15221e8463771b47de56fd9e9c
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:27:48 +0530

    Update question7.java

--- 15. Java IO/question7.java
+++ 15. Java IO/question7.java
@@ -1,1 +1,32 @@
 // 7. Read text from a .txt file using BufferedReader
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class question7 {
+    public static void main(String[] args) {
+
+        BufferedReader objReader = null;
+        try {
+            String strCurrentLine;
+            objReader = new BufferedReader(new FileReader("MyFile.txt"));
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                System.out.println(strCurrentLine);
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        } 
+        finally {
+            try {
+                if (objReader != null)
+                    objReader.close();
+            } 
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }//End main
+}//End question7 class
