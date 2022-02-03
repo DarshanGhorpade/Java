ae9393086d45fca8ca71ab6ba2ed070a0f6e3d10
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 11:25:20 +0530

    Update question2.java

--- 11. this and super/question2.java
+++ 11. this and super/question2.java
@@ -1,1 +1,20 @@
 // 2. Print the fields/instance members of the parent class using super
+
+class Animal{  
+    String color="white";  
+}  
+class Dog extends Animal{  
+    String color="black";  
+
+    void printColor(){  
+        System.out.println(color);//prints color of Dog class  
+        System.out.println(super.color);//prints color of Animal class  
+    }  
+}  
+
+class question2{  
+    public static void main(String args[]){  
+        Dog d=new Dog();  
+        d.printColor();  
+    }//End main
+}//End question2 class  
