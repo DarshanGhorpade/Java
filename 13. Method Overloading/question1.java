2c838dddde899ed3a4397768a01c0cd2b46f40fb
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 12:39:40 +0530

    Create question1.java

--- 13. Method Overloading/question1.java
+++ 13. Method Overloading/question1.java
@@ -1,1 +1,16 @@
 // 1. Write two methods with the same name but different number of parameters of same type and call the methods from main method
+
+class Adder{  
+    static int add(int a,int b){
+        return a+b;
+    }  
+    static int add(int a,int b,int c){
+        return a+b+c;
+    }  
+}  
+class question1{  
+   public static void main(String[] args){  
+        System.out.println(Adder.add(11,11));  
+        System.out.println(Adder.add(11,11,11));  
+    }//End main
+}//End question1 class  
