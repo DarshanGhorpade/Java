9d041e28d93805bac7f0eb3d22327b97245e1154
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 19:13:13 +0530

    Update question1.java

--- 14. Exceptions/question1.java
+++ 14. Exceptions/question1.java
@@ -1,1 +1,7 @@
 // 1. Write a program to generate Arithmetic Exception without exception handling
+
+public class question1{
+    public static void main(String[]args){
+        System.out.println("10/0 = "+ 10/0);
+    }//End main
+}//End question1 class
