cec139822350b811375de5cf88693108eacb8c9f
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-02-03 15:09:49 +0530

    Update question4.java

--- 15. Java IO/question4.java
+++ 15. Java IO/question4.java
@@ -1,1 +1,29 @@
 // 4. Write text to a .txt file using BufferedOutputStream
+
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+
+public class question4 {
+    public static void main(String[] args) {
+
+        String data = "This is a line of text inside the file";
+
+        try {
+            // Creates a FileOutputStream
+            FileOutputStream file = new FileOutputStream("MyFile.txt");
+
+            // Creates a BufferedOutputStream
+            BufferedOutputStream output = new BufferedOutputStream(file);
+
+            byte[] array = data.getBytes();
+
+            // Writes data to the output stream
+            output.write(array);
+            output.close();
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }//End main
+}//End question4 class
